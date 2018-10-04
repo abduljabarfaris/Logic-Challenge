8b574940a04e2777751ab82962b5a0f1ad8915f7
abduljabarfaris <42913153+abduljabarfaris@users.noreply.github.com>
2018-10-04 11:57:43 +0700

    Update bandingkanangka.js

--- bandingkanangka.js
+++ bandingkanangka.js
@@ -10,5 +10,3 @@ function bandingkanangka(angka1,angka2) {
 console.log(bandingkanangka(5, 8)); // true
 console.log(bandingkanangka(5, 3)); // false
 console.log(bandingkanangka(4, 4)); // -1
-console.log(bandingkanangka(3, 3)); // -1
-console.log(bandingkanangka(17, 2)); // false
