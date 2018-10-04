a573929a457c6765a576c3a7c1128e0b5f49d34d
abduljabarfaris <42913153+abduljabarfaris@users.noreply.github.com>
2018-10-04 14:15:20 +0000

    Update XdanO.js

--- XdanO.js
+++ XdanO.js
@@ -1,17 +1,19 @@
 function xo(str) {
-  var tX=0;
-  var tO=0;
-  for (var x = 0; x <str.length; x++){
-    if(str[x] === 'x'){
-      tX ++;
-    } else if (str[x] === 'o'){
-      tO ++;
+  var X=0;
+  var O=0;
+  for (var i = 0; i <str.length; i++){
+    if(str[i] < 'x'){
+      X ++;
+    } else if (str[i] > 'o'){
+      O ++;
     }
-  return X === O;
-}
+   }
+  return X != O ? false : true;
+ } 
 
-// TEST CASES
+//TEST CASES
 console.log(xo('xoxoxo')); // true
 console.log(xo('oxooxo')); // false
 console.log(xo('oxo')); // false
 console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
